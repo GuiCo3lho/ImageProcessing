ebd593a213b67df2377d78c662db14e8d683ddba
GuiCo3lho <37370101+GuiCo3lho@users.noreply.github.com>
2018-06-13 11:39:42 -0300

    Projeto Toonify - ImageProcessing
    1- Foi criado a função para converter rgb para gray
    2- Criado codigo para busca do arquivo de fotos 
    3- Teste com o median filter de matriz 7x7 com imagens gray

---
+++ Trabalho Final IPI - Toonify/Codigo/Preparation.m
@@ -0,0 +1,28 @@
+close all;
+clear all.
+clc;
+% Buscando diretório de imagens originais
+myFolder = 'D:\MATLAB\IPI\Trabalho Final IPI - Toonify\ImagensTeste';
+% Verificar se o diretorio existe
+if ~isfolder(myFolder)
+    sprintf('Error: Folder %s does not exist', myFolder);
+    return;
+end
+% Procurando apenas por arquivos do tipo 'jpg'
+filepadrao = fullfile(myFolder,'*.jpg');
+Diretorio = dir(filepadrao);
+
+% Varrendo cada imagem no diretório
+for posfile = 1:length(Diretorio)
+    imagemOriginal = imread(Diretorio(posfile).name);
+    figure, imshow(imagemOriginal);
+    imagemycbcr = rgbtogray(imagemOriginal); %YCbCr components of the image
+    imagemGray = imagemycbcr(:,:,1); %pegar Luminance component
+    figure, imshow(imagemGray);
+    fmgrayimage = medfilt2(imagemGray, [7 7]);%filtragem 7x7 com median filter
+    figure, imshow(fmgrayimage);
+end
+
+    
+    
+
