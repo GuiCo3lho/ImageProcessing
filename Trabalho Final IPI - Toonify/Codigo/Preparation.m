1096f36ff397537173d5a8b2e4d4ddaf8e288192
GuiCo3lho <37370101+GuiCo3lho@users.noreply.github.com>
2018-06-13 14:41:23 -0300

    Codigo gray_medianfilter
    Corrigido o problema para buscar arquivo sem usar o path todo

--- Trabalho Final IPI - Toonify/Codigo/Preparation.m
+++ Trabalho Final IPI - Toonify/Codigo/Preparation.m
@@ -1,27 +1,22 @@
 close all;
-clear all;
+clear all.
 clc;
-% Buscando diretório de imagens originais
-myFolder = 'D:\MATLAB\IPI\Trabalho Final IPI - Toonify\ImagensTeste';
-% Verificar se o diretorio existe
-if ~isfolder(myFolder)
-    sprintf('Error: Folder %s does not exist', myFolder);
-    return;
-end
-% Procurando apenas por arquivos do tipo 'jpg'
-filepadrao = fullfile(myFolder,'*.jpg');
-Diretorio = dir(filepadrao);
+cd ../ImagensTeste/;
+Diretorio = dir('*.jpg');
 
-% Varrendo cada imagem no diretório
+% Varrendo cada imagem no diret�rio
 for posfile = 1:length(Diretorio)
     imagemOriginal = imread(Diretorio(posfile).name);
     figure, imshow(imagemOriginal);
     imagemycbcr = rgbtogray(imagemOriginal); %YCbCr components of the image
     imagemGray = imagemycbcr(:,:,1); %pegar Luminance component
     figure, imshow(imagemGray);
-    fmgrayimage = medfilt2(imagemGray, [7 7]);%filtragem 7x7 com median filter
+    fmgrayimage = medfilt2(imagemGray, [7 7]);
     figure, imshow(fmgrayimage);
+    impixelinfo;
 end
+cd ..;
+cd Codigo;
 
     
     
