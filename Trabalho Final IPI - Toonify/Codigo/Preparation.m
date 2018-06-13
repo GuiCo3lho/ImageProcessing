8851b9c65ea95bbb2f9aa8041ea6cb2a655cc90a
GuiCo3lho <37370101+GuiCo3lho@users.noreply.github.com>
2018-06-13 19:17:06 -0300

    Main file
    Adicionei código para retornar os outputs do código para a pasta "ImagensCartoon"

--- Trabalho Final IPI - Toonify/Codigo/Preparation.m
+++ Trabalho Final IPI - Toonify/Codigo/Preparation.m
@@ -11,12 +11,18 @@ for posfile = 1:length(Diretorio)
     %figure, imshow(imagemOriginal);
     imagemycbcr = rgbtogray(imagemOriginal); %YCbCr components of the image
     imagemGray = imagemycbcr(:,:,1); %pegar Luminance component
-    figure, imshow(imagemGray);
-    fmgrayimage = medfilt2(imagemGray, [7 7]);
-    figure, imshow(fmgrayimage);
+    %figure, imshow(imagemGray);
+    fmgrayimage = medfilt2(imagemGray, [7 7]);%usando o median filter na grayscale image
+    %figure, imshow(fmgrayimage);
     pegabordas = edge(fmgrayimage,'Canny',[0.1 0.4]);
-    figure, imshow(pegabordas);   
+    %figure, imshow(pegabordas); 
+    
+    
+     DiretorioResultado = sprintf('../ImagensCartoon/cartoon_%d.jpg', posfile);  
+     imwrite(pegabordas, DiretorioResultado);
+    
 end
+
 cd ..;
 cd Codigo;
 
