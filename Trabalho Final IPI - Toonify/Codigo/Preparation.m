c72499001036ba4394ecdf14dee361f6c0e83479
GuiCo3lho <37370101+GuiCo3lho@users.noreply.github.com>
2018-06-13 17:49:22 -0300

    Edge detection começo
    Adicionei função de edge detection com canny e testei valores de threshold

--- Trabalho Final IPI - Toonify/Codigo/Preparation.m
+++ Trabalho Final IPI - Toonify/Codigo/Preparation.m
@@ -5,15 +5,17 @@ cd ../ImagensTeste/;
 Diretorio = dir('*.jpg');
 
 % Varrendo cada imagem no diretório
+%for posfile = 1:length(Diretorio)
 for posfile = 1:length(Diretorio)
     imagemOriginal = imread(Diretorio(posfile).name);
-    figure, imshow(imagemOriginal);
+    %figure, imshow(imagemOriginal);
     imagemycbcr = rgbtogray(imagemOriginal); %YCbCr components of the image
     imagemGray = imagemycbcr(:,:,1); %pegar Luminance component
     figure, imshow(imagemGray);
     fmgrayimage = medfilt2(imagemGray, [7 7]);
     figure, imshow(fmgrayimage);
-    impixelinfo;
+    pegabordas = edge(fmgrayimage,'Canny',[0.1 0.4]);
+    figure, imshow(pegabordas);   
 end
 cd ..;
 cd Codigo;
