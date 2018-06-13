937ac2c7cc8fb43742c19e501c478d52437a27d9
GuiCo3lho <37370101+GuiCo3lho@users.noreply.github.com>
2018-06-13 12:21:21 -0300

    Função main
    Codigo sem o path name do arquivo
    Não consegui usar o "cd" para encontrar o arquivo sem usar o path todo do pc no matlab

---
+++ Trabalho Final IPI - Toonify/Codigo/Preparation2.m
@@ -0,0 +1,20 @@
+close all;
+clear all.
+clc;
+
+Diretorio = dir('*jpg');
+
+% Varrendo cada imagem no diretório
+for posfile = 1:length(Diretorio)
+    imagemOriginal = imread(Diretorio(posfile).name);
+    figure, imshow(imagemOriginal);
+    imagemycbcr = rgbtogray(imagemOriginal); %YCbCr components of the image
+    imagemGray = imagemycbcr(:,:,1); %pegar Luminance component
+    figure, imshow(imagemGray);
+    fmgrayimage = medfilt2(imagemGray, [7 7]);
+    figure, imshow(fmgrayimage);
+end
+
+    
+    
+
