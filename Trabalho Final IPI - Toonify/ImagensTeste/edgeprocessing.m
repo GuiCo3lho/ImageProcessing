47228b0b0bdeec9b79cb7a90608c0a0b5d1f4285
vitorbandeira <vitorbandeira@cjr.org.br>
2018-06-19 16:50:06 -0300

    quase lá

--- Trabalho Final IPI - Toonify/ImagensTeste/edgeprocessing.m
+++ Trabalho Final IPI - Toonify/ImagensTeste/edgeprocessing.m
@@ -1,6 +1,7 @@
 function finaledges = edgeprocessing(image)
     pegabordas = edge(image,'Canny',[0.07 0.2]);
-    dilatabordas = imdilate(pegabordas,strel('square',2));%dilatar as bordas conseguidas no edge detection "Canny"
-    blackedgesdilated = imcomplement(dilatabordas); %inverter a imagem "dilatabordas" para tornar as bordas pretas
+    bordasDilatadas = imdilate(pegabordas,strel('square',2));%dilatar as bordas conseguidas no edge detection "Canny"
+    filtredEdges = bwareaopen(bordasDilatadas,300,8);%filtro de bordas que apaga bordas com tamanho menor ou igual a 400 pixels
+    blackedgesdilated = imcomplement(filtredEdges); %inverter a imagem "dilatabordas" para tornar as bordas pretas
     finaledges = blackedgesdilated;
 end
