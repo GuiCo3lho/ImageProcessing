b52c4edcbf070e681c0b89a55f518162db47bbf0
vitorbandeira <vitorbandeira@cjr.org.br>
2018-06-19 16:14:27 -0300

    quase lá

--- Trabalho Final IPI - Toonify/Codigo/Principal.m
+++ Trabalho Final IPI - Toonify/Codigo/Principal.m
@@ -11,16 +11,26 @@ for posfile = 1:length(Diretorio)
     imagemOriginal = imread(Diretorio(posfile).name);%ler cada foto do arquivo
     imagemycbcr = rgbtogray(imagemOriginal); %YCbCr components of the image
     imagemGray = imagemycbcr(:,:,1); %pegar Luminance component
-    
+    [h, w, c] = size(imagemOriginal);
     fmgrayimage = medfilt2(imagemGray, [7 7]);%usando o median filter na grayscale image
    
     finaledges = edgeprocessing(fmgrayimage); %processamento de edges
     
     imageAddEdges = immultiply(imagemGray,finaledges);%por as bordas feitas no edgeprocessing na imagem gray
     
+  
+    color = ColorPreparation(imagemOriginal);
+    
+    edges=zeros(h,w,c,'uint8');
+    edges(:,:,1)=finaledges;
+    edges(:,:,2)=finaledges;
+    edges(:,:,3)=finaledges;
+    
+    final=immultiply(edges,color);    
+    
     %transferindo para pasta de ImagensCartoon os resultados
     DiretorioResultado = sprintf('../ImagensCartoon/cartoon_%d.jpg', posfile);  
-    imwrite(finaledges, DiretorioResultado);
+    imwrite(final, DiretorioResultado);
     
 end
 
